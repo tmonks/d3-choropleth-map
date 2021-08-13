14ef58df2a9be2dcfa68c21491dd2bf380116607
Tom Monks <monksy@gmail.com>
2021-08-13 09:58:19 -0400

    First draft of legend

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const w = 1400;
 const h = 1000;
 const legendWidth = 300;
-const legendHeight = 20;
+const legendHeight = 40;
+const legendMargin = { top: 0, right: 0, bottom: 20, left: 0 };
 const margin = { top: 100, right: 50, bottom: 50, left: 50 };
 const graphWidth = w - margin.left - margin.right;
 const graphHeight = h - margin.top - margin.bottom;
@@ -16,9 +17,6 @@ const graph = svg
   .attr("width", graphWidth)
   .attr("height", graphHeight)
   .attr("transform", `translate(${margin.left}, ${margin.top})`); // move it by margin sizes
-
-// create legend area
-// const legend = svg.append("g").attr("width", legendWidth).attr("height", legendHeight).attr("id", "legend");
 
 // add title
 svg
@@ -36,7 +34,32 @@ svg
   .attr("y", 60)
   .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
 
-color = d3.scaleQuantize([0, 100], d3.schemeBlues[9]);
+const countyColors = d3.schemeBlues[9];
+colorScale = d3.scaleQuantize([0, 100], countyColors);
+
+// legend
+const legendScale = d3.scaleLinear().domain([0, 100]).range([0, legendWidth]);
+const legendAxis = d3.axisBottom(legendScale);
+const legend = svg
+  .append("g")
+  .attr("width", legendWidth)
+  .attr("height", legendHeight)
+  .attr("transform", `translate(0, ${h - legendHeight})`)
+  .attr("id", "legend");
+legend
+  .append("g")
+  .attr("id", "legend-axis")
+  .attr("transform", `translate(${legendMargin.left}, ${legendHeight - legendMargin.bottom})`)
+  .call(legendAxis);
+legend
+  .selectAll("rect")
+  .data(countyColors)
+  .enter()
+  .append("rect")
+  .attr("width", legendWidth / countyColors.length) // divided up into equal sized rects for each color in the scale
+  .attr("height", legendHeight - legendMargin.bottom - legendMargin.top)
+  .attr("x", (d, i) => (legendWidth / countyColors.length) * i)
+  .attr("fill", (d) => d);
 
 const promises = [
   d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"),
@@ -61,6 +84,8 @@ Promise.all(promises).then((values) => {
     .append("path")
     .attr("class", "county")
     .attr("d", path)
-    .attr("fill", (d) => color(data.find((x) => d.id == x.fips).bachelorsOrHigher))
-    .attr("stroke", "white");
+    .attr("fill", (d) => colorScale(data.find((x) => d.id == x.fips).bachelorsOrHigher))
+    .attr("stroke", "white")
+    .attr("data-fips", (d) => data.find((x) => d.id == x.fips).fips)
+    .attr("data-education", (d) => data.find((x) => d.id == x.fips).bachelorsOrHigher);
 });
